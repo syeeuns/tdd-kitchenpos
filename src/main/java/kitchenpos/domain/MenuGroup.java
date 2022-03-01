29d9e0b3715afaba9c9b27b017bd142070bc66eb
syeeuns <syeeuns@gmail.com>
2022-03-01 17:21:05 +0900

    Menu, MenuGroup, MenuProduct, Product Builder 생성

--- src/main/java/kitchenpos/domain/MenuGroup.java
+++ src/main/java/kitchenpos/domain/MenuGroup.java
@@ -1,11 +1,11 @@
 package kitchenpos.domain;
 
 import java.util.Objects;
+import java.util.UUID;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import java.util.UUID;
 
 @Table(name = "menu_group")
 @Entity
@@ -23,6 +23,29 @@ public class MenuGroup {
     public MenuGroup(UUID id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public MenuGroup(Builder builder) {
+        this.id = builder.id;
+        this.name = builder.name;
+    }
+
+    public static class Builder {
+        private UUID id;
+        private String name;
+
+        public Builder() {
+        }
+
+        public Builder id(UUID id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
     }
 
     public UUID getId() {
